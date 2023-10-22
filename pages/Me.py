fe3a99fa65f69e75c41acd110e93fc0e52d1ddcd
Chibuike Onuba <103614409+ChibuikeOnuba@users.noreply.github.com>
2023-10-22 17:14:02 +0100

    Add files via upload

--- pages/Me.py
+++ pages/Me.py
@@ -5,8 +5,9 @@ from PIL import Image
 def main():
     st.title('About Me')
     col1, col2 = st.columns(2)
-    image = Image.open(r"C:\Users\HP\Documents\ML Projects\Diamond Price prediction\pages\my_picture.jpg")
-    col1.image(image, width=225)
+    #image = Image.open(r"C:\Users\HP\Documents\ML Projects\Diamond Price prediction\pages\my_picture.jpg")
+    #ol1.image(image, width=225)
+    col1.image('my_picture.jpg')
     
     col2.write('I am a Data Scientist with passion for building ML solutions and making visuals to analyze business insights.') 
                
