6d0b44b501ea7c3bc94511e45cb6a11e671607bc
Chibuike Onuba <103614409+ChibuikeOnuba@users.noreply.github.com>
2023-10-22 17:43:20 +0100

    Update Me.py

--- pages/Me.py
+++ pages/Me.py
@@ -12,13 +12,20 @@ def main():
     col2.write('I am a Data Scientist with passion for building ML solutions and making visuals to analyze business insights.') 
                
     col2.write('I am in my penultimate year studying Computer Science major at the University of Nigeria')
-    col2.write('Feel free to go through my projects :)')
+    col2.write('Feel free to go through my projects and contact me for collaborations :)')
+    
     
     
     
     expander = st.expander("check out my other projects")
     expander.selectbox(label='select', options=['Diabetes Prediction', 'Covid 19 prediction', 'Iris Classification', 'Customer Segmentation'])  
-    st.button('Contact Me')
+    st.write('Visit my personal website')
+    if st.button("Visit Portfolio"):
+    # Replace 'https://chibuikeonuba/github.io' with the URL you want to visit
+        st.markdown(
+            f'<a href="https://chibuikeonuba.github.io" target="_blank">Portfolio Site</a>',
+            unsafe_allow_html=True
+        )
 
 if __name__ == '__main__':
     main()
